db88dd9c05f59c7fbdf1542cf6cfb03a9a209613
me <DTother@seznam.cz>
2019-04-04 22:30:07 +0200

    Vulkan level implementation PART #7     added structure, union and info translator + templates

--- cz/mg/vulkantransformator/translators/vulkan/VulkanInfoTranslator.java
+++ cz/mg/vulkantransformator/translators/vulkan/VulkanInfoTranslator.java
@@ -1,11 +1,26 @@
 package cz.mg.vulkantransformator.translators.vulkan;
 
+import cz.mg.collections.list.chainlist.CachedChainList;
+import cz.mg.collections.list.chainlist.ChainList;
 import cz.mg.vulkantransformator.entities.EntityTriplet;
+import cz.mg.vulkantransformator.entities.InfoTriplet;
+import cz.mg.vulkantransformator.entities.vulkan.VulkanVariable;
 
 
 public class VulkanInfoTranslator extends VulkanTranslator {
     @Override
     public String genCode(EntityTriplet e, String template) {
-        return null;
+        InfoTriplet entity = (InfoTriplet) e;
+        return super.genCode(e, template
+                .replace("%PARAMETERS%", VulkanStructureTranslator.genParameters(reduce(entity.getVulkan().getFields())))
+                .replace("%ARGUMENTS%", VulkanStructureTranslator.genArguments(reduce(entity.getVulkan().getFields())))
+                .replace("%PROPERTIES%", VulkanStructureTranslator.genProperties(entity.getVulkan().getFields()))
+        );
+    }
+
+    private ChainList<VulkanVariable> reduce(ChainList<VulkanVariable> fields){
+        ChainList<VulkanVariable> reduced = new CachedChainList<>(fields);
+        reduced.removeFirst();
+        return reduced;
     }
 }
