14d759d97f23081a382964ee645c101ca423375a
Me <DTother@seznam.cz>
2019-04-22 14:49:29 +0200

    fixed info arrays - forgot to initialize structure type

--- cz/mg/vulkantransformator/translators/vk/VkInfoTranslator.java
+++ cz/mg/vulkantransformator/translators/vk/VkInfoTranslator.java
@@ -4,16 +4,19 @@ import cz.mg.collections.list.chainlist.ChainList;
 import cz.mg.collections.text.Text;
 import cz.mg.vulkantransformator.entities.vk.VkEntity;
 import cz.mg.vulkantransformator.entities.vk.VkInfo;
+import cz.mg.vulkantransformator.translators.vk.templates.TemplatesVk;
 
 
 public class VkInfoTranslator extends VkTranslator {
-    private static final Text setTemplateVk = new Text("        setSType(new VkStructureType(VkStructureType.VK_STRUCTURE_TYPE_%VKNAMEUPPER%));");
+    private static final Text setTemplateVk = new Text("setSType(new VkStructureType(VkStructureType.VK_STRUCTURE_TYPE_%VKNAMEUPPER%));");
+    private static final Text infoArrayTemplate = TemplatesVk.load("parts/InfoArray");
 
     @Override
     public Text genCode(ChainList<VkEntity> entities, VkEntity e, Text template) {
         VkInfo vk = (VkInfo) e;
         return super.genCode(entities, e, template
                 .replace("%PROPERTIES%", VkStructureTranslator.genPropertiesVk(entities, vk.getFields()))
+                .replace("%%ARRAY%%", infoArrayTemplate)
                 .replace("%SET%", genSet(vk))
         );
     }
